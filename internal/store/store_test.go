52e46dcc80042551858f7ebad2de2089e4d35375
Tushig <tushig.munkhbayar@outlook.com>
2026-05-21 01:17:00 +0200

    get mutation test

--- internal/store/store_test.go
+++ internal/store/store_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestStore(t *testing.T) {
+func TestSetAndGet(t *testing.T) {
 	rune := NewRune()
 
 	key := "aa"
@@ -19,3 +19,21 @@ func TestStore(t *testing.T) {
 		t.Errorf("Invalid length")
 	}
 }
+
+func TestGetDoesNotAllowMutation(t *testing.T) {
+	rune := NewRune()
+
+	key := "aa"
+	val := []byte("value")
+	rune.Set(key, val)
+	got := rune.Get("aa")
+	got[0] = 't'
+	got2 := rune.Get("aa")
+	if string(got2) != "value" {
+		t.Errorf("invalid value!")
+	}
+}
+
+func TestConcurrentSetGet(t *testing.T) {}
+
+func TestDelete(t *testing.T) {}
