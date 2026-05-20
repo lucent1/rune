6c71d4c0a7306b5e8a1efe5d6036bf681e2483dd
Tushig <tushig.munkhbayar@outlook.com>
2026-05-20 23:29:00 +0200

    Basic handler

--- internal/api/handler.go
+++ internal/api/handler.go
@@ -1,8 +1,10 @@
 package api
 
 import (
+	"io"
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/lucent1/rune/internal/store"
 )
 
@@ -17,9 +19,22 @@ func NewHandler(rune *store.Rune) *Handler {
 }
 
 func (h *Handler) Set(w http.ResponseWriter, r *http.Request) {
+	key := chi.URLParam(r, "key")
 
+	val, err := io.ReadAll(r.Body)
+	if err != nil || val == nil {
+		http.Error(w, "Failed to read body", http.StatusBadRequest)
+		return
+	}
+
+	h.rune.Set(key, val)
+
+	w.WriteHeader(http.StatusCreated)
 }
 
 func (h *Handler) Get(w http.ResponseWriter, r *http.Request) {
+	key := chi.URLParam(r, "key")
 
+	val := h.rune.Get(key)
+	w.Write(val)
 }
